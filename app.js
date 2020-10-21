af85149b577fdf3fbfd47eb5de18e4937e01baca
Kai Carver <kaicarver@gmail.com>
2020-10-21 23:31:44 +0200

    login page

--- app.js
+++ app.js
@@ -43,6 +43,10 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
 app.get('/register', (req, res) => {
     res.render('register');
 });
