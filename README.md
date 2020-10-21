229cab9231a342dc79acee2bd6eb1d78f06b4f9a
Kai Carver <kaicarver@gmail.com>
2020-10-21 20:58:49 +0200

    start with simplest Express app with handlebars

---
+++ README.md
@@ -0,0 +1,5 @@
+# authtest
+
+Simplest possible authentication with Express.
+
+Based on Janith Kasun's article [Handling Authentication in Express.js](https://stackabuse.com/handling-authentication-in-express-js/)
---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// To support URL-encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// To parse cookies from the HTTP Request
+app.use(cookieParser());
+
+app.engine('hbs', exphbs({
+    extname: '.hbs'
+}));
+
+app.set('view engine', 'hbs');
+
+// Our requests handlers will be implemented here...
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.listen(3000);
