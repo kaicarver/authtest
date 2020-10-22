e12b193bc042c3cbd657cd475b5c6d3d241d56ef
Kai Carver <kaicarver@gmail.com>
2020-10-22 18:05:05 +0200

    add GET for all books

--- api.js
+++ api.js
@@ -14,6 +14,10 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.get('/book', (req, res) => {
+  res.json(books);
+});
+
 app.post('/book', (req, res) => {
   const book = req.body;
 
